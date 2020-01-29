146f3935b0152822944ee8d9755d150b36963ab2
OwOHuB <36792466+owo-hub@users.noreply.github.com>
2020-01-29 14:36:10 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import discord
 import os
 
-
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
@@ -11,13 +11,17 @@ async def on_ready():
     print(client.user.id)
     print("----------------")
     await client.change_presence(game=discord.Game(name='', type=1))
-    
+
+
 @client.event
 async def on_message(message):
     if message.content.startswith("hi"):
         await client.send_message(message.channel, "HI")
 
-
+    if message.content.startswith("/dm"):
+        author = message.guild.get_member(int(message.content[4:22]))
+        msg = message.content[23:]
+        await author.send(msg)
 
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
