2d29b086a338360d16c8386025c724b44739761c
OwOHuB <36792466+owo-hub@users.noreply.github.com>
2020-01-29 13:46:14 +0900

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,23 @@
+import discord
+import os
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready();
+    print("login")
+    print(client.user.name)
+    print(client.user.id)
+    print("----------------")
+    await client.change_presence(game=discord.Game(name='', type=1))
+    
+@client.event
+async def on_message(message):
+    if message.content.startswith("hi"):
+        await client.send_message(message.channel, "HI")
+
+
+
+access_token = os.environ["BOT_TOKEN"]
+client.run(access_token)
